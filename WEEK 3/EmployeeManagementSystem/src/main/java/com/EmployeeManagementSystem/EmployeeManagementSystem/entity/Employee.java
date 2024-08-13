8a64e02be305c87efd0c122b6cecd084ebff5dd4
Issac-31 <issacnewton934@gmail.com>
2024-08-13 20:26:04 +0530

    task5

--- WEEK 3/EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/entity/Employee.java
+++ WEEK 3/EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/entity/Employee.java
@@ -4,12 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import lombok.Data;
 
 @Entity
 @Data
+@NamedQueries({
+    @NamedQuery(
+        name = "Employee.findByDepartmentId",
+        query = "SELECT e FROM Employee e WHERE e.department.id = :departmentId"
+    ),
+    @NamedQuery(
+        name = "Employee.findByNameContaining",
+        query = "SELECT e FROM Employee e WHERE e.name LIKE %:name%"
+    )
+})
 public class Employee {
 
     @Id
--- WEEK 3/EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ WEEK 3/EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.EmployeeManagementSystem.EmployeeManagementSystem.repository;
 
 import com.EmployeeManagementSystem.EmployeeManagementSystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +10,8 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    // Derived query methods
-    List<Employee> findByDepartmentId(Long departmentId);
-    
-    List<Employee> findByNameContaining(String name);
+    // Using named queries defined in the Employee entity
+    List<Employee> findByDepartmentId(@Param("departmentId") Long departmentId);
+
+    List<Employee> findByNameContaining(@Param("name") String name);
 }
